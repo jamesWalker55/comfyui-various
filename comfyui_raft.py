f092a680660ed264cdf23b212ffde8ea77e1ea6e
James Walker <james.chunho@gmail.com>
2023-08-06 23:42:08 +0100

    Load flows from EXR

--- comfyui_raft.py
+++ comfyui_raft.py
@@ -1,4 +1,6 @@
 import comfy.model_management as model_management
+import numpy as np
+import OpenEXR
 import torch
 import torchvision.transforms.functional as F
 from torchvision.models.optical_flow import Raft_Large_Weights, raft_large
@@ -194,3 +196,86 @@ class _:
         images = native_torch_to_comfyui(images)
 
         return (images,)
+
+
+def depth_exr_to_numpy(exr_path, typemap={"HALF": np.float16, "FLOAT": np.float32}):
+    # Code stolen from:
+    # https://gist.github.com/andres-fr/4ddbb300d418ed65951ce88766236f9c
+
+    # load EXR and extract shape
+    exr = OpenEXR.InputFile(exr_path)
+    print(exr.header())
+    dw = exr.header()["dataWindow"]
+    shape = (dw.max.y - dw.min.y + 1, dw.max.x - dw.min.x + 1)
+    #
+    arr_maps = {}
+    for ch_name, ch in exr.header()["channels"].items():
+        print("reading channel", ch_name)
+        # This, and __str__ seem to be the only ways to get typename
+        exr_typename = ch.type.names[ch.type.v]
+        np_type = typemap[exr_typename]
+        # convert channel to np array
+        bytestring = exr.channel(ch_name, ch.type)
+        arr = np.frombuffer(bytestring, dtype=np_type).reshape(shape)
+        arr_maps[ch_name] = arr
+
+    return arr_maps
+
+
+@register_node("RAFTLoadFlowFromEXRChannels", "RAFT Load Flow from EXR Channels")
+class _:
+    """
+    This is a utility function for loading motion flows from an EXR image file.
+    This is intended for use with Blender's vector pass in the Cycles renderer.
+
+    In Blender, enable the vector pass. In the compositor, use "Separate Color" to
+    extract the "Blue" and "Alpha" channels of the vector pass. Then, combine them
+    using "Combine Color" to two of the RGB channels. Finally, render to the "OpenEXR"
+    format.
+
+    https://gist.github.com/andres-fr/4ddbb300d418ed65951ce88766236f9c
+    """
+
+    CATEGORY = "jamesWalker55"
+
+    INPUT_TYPES = lambda: {
+        "required": {
+            "path": ("STRING", {"default": ""}),
+            "x_channel": (("R", "G", "B", "A"), {"default": "R"}),
+            "y_channel": (("R", "G", "B", "A"), {"default": "G"}),
+            "invert_x": (("false", "true"), {"default": "true"}),
+            "invert_y": (("false", "true"), {"default": "false"}),
+        }
+    }
+
+    RETURN_NAMES = ("RAFT_FLOW",)
+    RETURN_TYPES = ("RAFT_FLOW",)
+
+    OUTPUT_NODE = False
+
+    FUNCTION = "execute"
+
+    def execute(
+        self, path: str, x_channel: str, y_channel: str, invert_x: str, invert_y: str
+    ):
+        assert isinstance(path, str)
+        assert x_channel in ("R", "G", "B", "A")
+        assert y_channel in ("R", "G", "B", "A")
+        assert invert_x in ("true", "false")
+        assert invert_y in ("true", "false")
+
+        invert_x: bool = invert_x == "true"
+        invert_y: bool = invert_y == "true"
+
+        maps = depth_exr_to_numpy(path)
+
+        x = torch.from_numpy(maps[x_channel])
+        y = torch.from_numpy(maps[y_channel])
+
+        if invert_x:
+            x = x * -1
+
+        if invert_y:
+            y = y * -1
+
+        return (torch.stack((x, y)).unsqueeze(0),)
