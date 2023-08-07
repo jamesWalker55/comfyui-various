be0e7276dc9dd5969b5b594515f65ad132e9baf4
James Walker <james.chunho@gmail.com>
2023-08-07 12:01:59 +0100

    Hide nodes if environment variable is not set

--- __init__.py
+++ __init__.py
@@ -1,17 +1,30 @@
 import importlib
+import os
 
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
+# Main nodes for all users
 NODE_MODULES = [
-    ".comfyui_batch_io",
-    ".comfyui_group_io",
     ".comfyui_image_ops",
     ".comfyui_primitive_ops",
     ".comfyui_raft",
     ".comfyui_image_channel_ops",
     ".comfyui_color_ops",
 ]
+
+# Extra nodes for my own use
+if (
+    "COMFYUI_JW_ENABLE_EXTRA_NODES" in os.environ
+    and os.environ["COMFYUI_JW_ENABLE_EXTRA_NODES"].lower() == "true"
+):
+    NODE_MODULES.extend(
+        [
+            ".comfyui_batch_io",
+            ".comfyui_group_io",
+            ".comfyui_cn_preprocessors",
+        ]
+    )
 
 
 def load_nodes(module_name):
