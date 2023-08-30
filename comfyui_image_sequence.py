923be36af63488255f9cd75b376f02c58ed5c80c
James Walker <james.chunho@gmail.com>
2023-08-30 02:44:56 +0100

    rework image sequence loaders

--- comfyui_image_sequence.py
+++ comfyui_image_sequence.py
@@ -40,6 +40,7 @@ class _:
             ),
             "start_index": ("INT", {"default": 0, "min": 0, "step": 1}),
             "frame_count": ("INT", {"default": 16, "min": 1, "step": 1}),
+            "ignore_missing_images": (("false", "true"), {"default": "false"}),
         }
     }
 
@@ -50,15 +51,103 @@ class _:
 
     FUNCTION = "execute"
 
-    def execute(self, path_pattern: str, start_index: int, frame_count: int):
-        image_paths = [
-            path_pattern.format(i)
-            for i in range(start_index, start_index + frame_count)
-        ]
+    def execute(
+        self,
+        path_pattern: str,
+        start_index: int,
+        frame_count: int,
+        ignore_missing_images: str,
+    ):
+        ignore_missing_images: bool = ignore_missing_images == "true"
 
+        # generate image paths to load
+        image_paths = []
+        for i in range(start_index, start_index + frame_count):
+            try:
+                image_paths.append(path_pattern.format(i))
+            except KeyError:
+                image_paths.append(path_pattern.format(i=i))
+
+        if ignore_missing_images:
+            # remove missing images
+            image_paths = [p for p in image_paths if os.path.exists(p)]
+        else:
+            # early check for missing images
+            for path in image_paths:
+                if not os.path.exists(path):
+                    raise FileNotFoundError(f"Image does not exist: {path}")
+
+        if len(image_paths) == 0:
+            raise RuntimeError("Image sequence empty - no images to load")
+
+        imgs = []
         for path in image_paths:
-            if not os.path.exists(path):
-                raise FileNotFoundError(f"Image does not exist: {path}")
+            img = load_image(path)
+            # img.shape => torch.Size([1, 768, 768, 3])
+            imgs.append(img)
+
+        imgs = torch.cat(imgs, dim=0)
+
+        return (imgs,)
+
+
+@register_node(
+    "JWLoadImageSequenceWithStopIndex",
+    "Batch Load Image Sequence With Stop Index",
+)
+class _:
+    CATEGORY = "jamesWalker55"
+
+    INPUT_TYPES = lambda: {
+        "required": {
+            "path_pattern": (
+                "STRING",
+                {"default": "./frame{:06d}.png", "multiline": False},
+            ),
+            "start_index": ("INT", {"default": 0, "min": 0, "step": 1, "max": 999999}),
+            "stop_index": ("INT", {"default": 16, "min": 1, "step": 1, "max": 999999}),
+            "inclusive": (("false", "true"), {"default": "false"}),
+            "ignore_missing_images": (("false", "true"), {"default": "false"}),
+        }
+    }
+
+    RETURN_NAMES = ("IMAGE",)
+    RETURN_TYPES = ("IMAGE",)
+
+    OUTPUT_NODE = False
+
+    FUNCTION = "execute"
+
+    def execute(
+        self,
+        path_pattern: str,
+        start_index: int,
+        stop_index: int,
+        inclusive: str,
+        ignore_missing_images: str,
+    ):
+        inclusive: bool = inclusive == "true"
+        ignore_missing_images: bool = ignore_missing_images == "true"
+
+        # generate image paths to load
+        image_paths = []
+        for i in range(start_index, stop_index + 1 if inclusive else stop_index):
+            try:
+                image_paths.append(path_pattern.format(i))
+            except KeyError:
+                image_paths.append(path_pattern.format(i=i))
+
+        if ignore_missing_images:
+            # remove missing images
+            image_paths = [p for p in image_paths if os.path.exists(p)]
+        else:
+            # early check for missing images
+            for path in image_paths:
+                if not os.path.exists(path):
+                    raise FileNotFoundError(f"Image does not exist: {path}")
+
+        if len(image_paths) == 0:
+            raise RuntimeError("Image sequence empty - no images to load")
 
         imgs = []
         for path in image_paths:
